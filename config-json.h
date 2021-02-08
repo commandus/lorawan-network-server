f8cb2f6bfc1a283c721d550be7bbb43ee9d0dc70
Andrei Ivanov <andrei.i.ivanov@commandus.com>
2021-02-08 14:46:28 +0900

    add identity service

---
+++ config-json.h
@@ -0,0 +1,40 @@
+#ifndef CONFIG_JSON_H
+#define CONFIG_JSON_H 1
+
+#include <string>
+#include <vector>
+
+#include "rapidjson/document.h"
+
+class ServerConfig {
+	public:
+		std::vector<std::string> listenAddressIPv4;
+		std::vector<std::string> listenAddressIPv6;
+		int readBufferSize;
+		int verbosity;
+		bool daemonize;
+		void clear();
+		ServerConfig();
+		int parse(
+			rapidjson::Value &value
+		);
+		void toJson(
+			rapidjson::Value &value,
+			rapidjson::Document::AllocatorType& allocator
+		);
+};
+
+class Configuration {
+	public:
+		std::string configFileName;
+		ServerConfig serverConfig;
+		Configuration();
+		Configuration(const char* value);
+		int parse(
+			const char* json
+		);
+		void clear();
+		std::string toString();
+};
+
+#endif
