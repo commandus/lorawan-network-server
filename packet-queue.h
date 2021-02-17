c49e5dffd7c3f07c76110a85814b9ff0e86889f4
Andrei Ivanov <andrei.i.ivanov@commandus.com>
2021-02-17 15:40:25 +0900

    Fix EUI string

---
+++ packet-queue.h
@@ -0,0 +1,50 @@
+#ifndef PACKET_QUEUE_H
+#define PACKET_QUEUE_H 1
+
+
+#include <deque>
+#include <vector>
+#include <map>
+#include <thread>
+#include <time.h>
+
+#include "utillora.h"
+
+// minimum delay in ms
+#define MIN_DELAY_MS 200
+// maximum delay in ms
+#define MAX_DELAY_MS 1000
+
+class SemtechUDPPacketItem {
+	public:
+		struct timeval timeAdded;
+		semtechUDPPacket packet;
+		DEVADDRINT getAddr() const;
+		SemtechUDPPacketItem(const semtechUDPPacket &packet);
+		std::string toString() const;
+};
+
+class SemtechUDPPacketsAddr {
+	public:
+		std::vector <SemtechUDPPacketItem> packets;
+		std::string toString() const;
+};
+
+class PacketQueue {
+	private:
+		int delayMS;
+		std::thread t;
+	public:
+		std::map<DEVADDRINT, SemtechUDPPacketsAddr, DEVADDRINTCompare> packets;
+		std::deque <DEVADDRINT> addrs;
+		PacketQueue();
+		PacketQueue(int delayMs);
+		void put(const semtechUDPPacket &value);
+		bool getFirstExpired(semtechUDPPacket &retval, struct timeval &currenttime);
+		void start();
+		void stop();
+		int diffMS(struct timeval &t1, struct timeval &t2);
+		std::string toString() const;
+};
+
+#endif
