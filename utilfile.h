29b891c4cfe4637bc78e1b1af427bf1e8cc4d0a5
Andrei Ivanov <andrei.i.ivanov@commandus.com>
2021-04-21 12:23:12 +0900

    Add dir txt identity loader

---
+++ utilfile.h
@@ -0,0 +1,31 @@
+/*
+ * utilfile.h
+ */
+#ifndef UTIL_FILE_H
+#define UTIL_FILE_H     1
+
+#include <string>
+#include <vector>
+
+namespace config {
+	bool rmDir(const std::string &path);
+	bool rmFile(const std::string &fn);
+
+	/**
+	 * Return list of files in specified path
+	 * @param path
+	 * @param flags 0- as is, 1- full path, 2- relative (remove parent path)
+	 * @param retval can be NULL
+	 * @return count files
+	 * FreeBSD fts.h fts_*()
+	 */
+	size_t filesInPath
+	(
+		const std::string &path,
+		const std::string &suffix,
+		int flags,
+		std::vector<std::string> *retval
+	);
+}
+
+#endif
