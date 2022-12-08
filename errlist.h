209c95b2c1576399afc1eed41a68eb1591a9e6b1
commandus <andrei.i.ivanov@commandus.com>
2022-12-08 10:21:05 +0900

    Win compat

--- errlist.h
+++ errlist.h
@@ -3,9 +3,16 @@
 
 // syslog
 #ifdef _MSC_VER
-#define	LOG_ERR								1
-#define	LOG_INFO							2
-#define	LOG_DEBUG							3
+#define	LOG_EMERG	0	/* system is unusable */
+#define	LOG_ALERT	1	/* action must be taken immediately */
+#define	LOG_CRIT	2	/* critical conditions */
+#define	LOG_ERR		3	/* error conditions */
+#define	LOG_WARNING	4	/* warning conditions */
+#define	LOG_NOTICE	5	/* normal but significant condition */
+#define	LOG_INFO	6	/* informational */
+#define	LOG_DEBUG	7	/* debug-level messages */
+#else
+#include <syslog.h>
 #endif
 
 // width in chars
